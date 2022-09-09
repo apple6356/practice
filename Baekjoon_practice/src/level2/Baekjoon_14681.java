ff457ac04264345e6c38790a4c92dccf13f29e44
apple6356 <apple1418@naver.com>
2022-09-09 23:47:57 +0900

    0909

---
+++ Baekjoon_practice/src/level2/Baekjoon_14681.java
@@ -0,0 +1,24 @@
+package level2;
+
+import java.util.Scanner;
+
+// 사분면 구분
+public class Baekjoon_14681 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int x, y;
+		
+		while(true) {
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			
+			if(x>=-1000 && x<=1000 && x!=0 && y>=-1000 && y<=1000 && y!=0) break;
+		}
+		
+		if(x>0 && y>0) System.out.println("1");
+		else if(x<0 && y>0) System.out.println("2");
+		else if(x<0 && y<0) System.out.println("3");
+		else if(x>0 && y<0) System.out.println("4");
+	}
+}
---
+++ Baekjoon_practice/src/level2/Baekjoon_2884.java
@@ -0,0 +1,29 @@
+package level2;
+
+import java.util.Scanner;
+
+// 알람 시계 설정 시간보다 45 전으로 설정
+public class Baekjoon_2884 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int H, M;
+		
+		while(true) {
+			H = scanner.nextInt();
+			M = scanner.nextInt();
+			
+			if(H>=0 && H<=23 && M>=0 && M<=59) break;
+		}
+		
+		M = M - 45;
+		if(M < 0) {
+			M = M + 60;
+			if(H == 0) H = 23;
+			else H = H - 1;
+		}
+		else H = H - 1;
+		
+		System.out.println(H + " " + M);
+	}
+}
